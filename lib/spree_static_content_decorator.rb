71d503d3505fbd248505e9feab73011b20d0b4ab
Riccardo Degan <riccardo.degan@geekcups.comn>
2013-10-10 18:50:24 +0200

    Fix wrong delete

--- lib/spree_static_content_decorator.rb
+++ lib/spree_static_content_decorator.rb
@@ -1,3 +1,25 @@
-Spree::Core::Engine.routes.draw do
- # Add your extension routes here
+Spree::StaticPage.class_eval do
+  def self.matches?(request)
+    locale = if request.session.key?(:locale) && SpreeI18n::Config.supported_locales.include?(request.session[:locale].to_sym)
+      request.session[:locale]
+    else
+      Rails.application.config.i18n.default_locale || I18n.default_locale
+    end
+    slug = ::StaticPage::remove_spree_mount_point(request.fullpath)
+    #pages = Spree::Page.arel_table
+    Spree::Page.visible.by_slug(slug, locale).exists?
+  end
+end
+
+
+Spree::StaticRoot.class_eval do
+  def self.matches?(request)
+    locale = if request.session.key?(:locale) && SpreeI18n::Config.supported_locales.include?(request.session[:locale].to_sym)
+      request.session[:locale]
+    else
+      Rails.application.config.i18n.default_locale || I18n.default_locale
+    end
+    path = ::StaticPage::remove_spree_mount_point(request.fullpath)
+    path.nil? && Spree::Page.visible.by_slug(path.to_s, locale).first
+  end
 end
